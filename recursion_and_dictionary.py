0e82ab8b844cfe46ccc090de000a50a026f8dab7
koopa01 <koopayu@gmail.com>
2020-01-14 22:11:58 +0800

    delete script to recrusion.py

--- recursion_and_dictionary.py
+++ recursion_and_dictionary.py
@@ -71,14 +71,6 @@ def isPalindrome(s):
 # print(isPalindrome())
 
 # 字典 dicrionaries
-# names = ['Ana', 'John', 'Denise', 'Katy']
-# grade = ['B', 'A+', 'A', 'A']
-# course = [2.00, 6.0001, 20.002, 9.01]
-# def get_grade(student, name_list, grade_list, course_list):
-# 	i = name_list.index(student) 
-# 	grade = grade_list[i]
-# 	course = course_list[i]
-# 	return (course, grade)
 grades = {'Ana':'B', 'John':'A+', 'Denise':'A', 'Katy':'A'}
 print(grades['John'])
 grades['Sylvan'] = 'A'  #添加项（标签label+值）
